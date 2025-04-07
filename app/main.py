32f263cc3e896a04b51f5239a510ac3719176cf0
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-08 01:42:34 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from app.api import vendor
 from app.database import Base, engine
 from app.models.context import VendorContext
+from app.api import context
 
 app = FastAPI()
 
 app.include_router(vendor.router)
+app.include_router(context.router)
 
 @app.on_event("startup")
 async def startup():
