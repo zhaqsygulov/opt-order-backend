4eea96ab8ddfafbd65c540709dfcb0c7c839a9a8
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-07 23:55:48 +0500

    Create vendor_client.py

---
+++ app/services/vendor_client.py
@@ -0,0 +1,13 @@
+import httpx
+from app.models.context import GetEmployeeContextResponse
+
+BASE_URL = "https://apps-api.moysklad.ru/api/vendor/1.0"
+
+async def get_employee_context(auth: str, context_id: str) -> GetEmployeeContextResponse:
+    headers = {
+        "Authorization": auth
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{BASE_URL}/context/{context_id}", headers=headers)
+        response.raise_for_status()
+        return GetEmployeeContextResponse(**response.json())
