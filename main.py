6cfe9be65051a00c20a85fce015d23f678e20473
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-08 23:29:48 +0500

    8

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
+
 from fastapi import FastAPI
 from app.api import vendor
-from app.db.models import Base
-from app.db.session import engine
 
-app = FastAPI(
-    title="Opt Order Backend",
-    version="0.7.0"
-)
-
+app = FastAPI()
 app.include_router(vendor.router)
-
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
